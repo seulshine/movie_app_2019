9b50620a256db72fc007662cf3d61f798e1775cc
seulshine <seulshine@gmail.com>
2020-01-10 17:33:43 +0900

    before making movie app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Food({name, picture, rating}) {
-  return <div>
-    <h2>I like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img width="350px" height="350px" src={picture} alt={name}></img>
-  </div>;
-}
+class App extends React.Component {
+  state = {
+    count: 0
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired, // check type and exist
-  rating : PropTypes.number  // only check type.
-};
+  // Javascript
+  // setState를 할 때 마다 react는 다시 render 한다. 새로운 state로!
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
 
-
-const foodILike = [
-  {
-    id:1,
-    name: "bagle",
-    image:
-      "https://kep.itechegypt.com/wp1/wp-content/uploads/2019/08/Bagle-Butter.jpg"
-    , rating: 5
-  },
-  {
-    id:2,
-    name:"coffee",
-    image:"https://cdn-image.foodandwine.com/sites/default/files/1568907144/Coffee-National-Coffee-Day-FT-Blog0919.jpg"
-  
-    , rating: 4.6  
+  render() {
+    return <div>
+      <h1>The number is : {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
   }
-]
 
-
-function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-}
-
-function App() {
-  return (
-  <div>
-    {console.log(foodILike.map(renderFood))}
-    {foodILike.map(renderFood)}}    
-  </div>
-  );
 }
 
 export default App;
