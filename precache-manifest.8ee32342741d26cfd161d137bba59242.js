c80a27a8501e18e65c24e6e1f772a6ed5fb15379
yeseul <seulshine@gmail.com>
2020-01-12 17:26:09 +0900

    Updates

---
+++ precache-manifest.8ee32342741d26cfd161d137bba59242.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874c0687caaf0904c7e4d1f798c44043",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "0f971c0d89a333a3c34a",
+    "url": "/movie_app_2019/static/css/main.a01281a6.chunk.css"
+  },
+  {
+    "revision": "808b925b5419023a8b06",
+    "url": "/movie_app_2019/static/js/2.3b871e1a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2019/static/js/2.3b871e1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f971c0d89a333a3c34a",
+    "url": "/movie_app_2019/static/js/main.db9084dd.chunk.js"
+  },
+  {
+    "revision": "c2db3e466beea69606d1",
+    "url": "/movie_app_2019/static/js/runtime-main.605c0ca1.js"
+  }
+]);
